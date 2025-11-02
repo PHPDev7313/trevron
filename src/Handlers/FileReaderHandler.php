571207f23f182cb442c5214e783b2238760f5c48
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2025-11-02 15:25:06 -0500

    Update File Reader Handler

--- src/Handlers/FileReaderHandler.php
+++ src/Handlers/FileReaderHandler.php
@@ -20,31 +20,47 @@ class FileReaderHandler
         $data = [];
 
         if (!is_dir($this->directory)) {
-            throw new HttpRuntimeException("Directory '{$this->directory}' does not exist");
+            $data[] = [
+                'success' => false,
+                'filename' => null,
+                'content' => null,
+                'error' => "Directory '{$this->directory}' does not exist!"
+            ];
+            return $data;
         }
 
+        /**
+         * glob returns array | false
+         */
         $files = glob($this->directory . '/*.json');
-        foreach ($files as $file) {
-            $contents = file_get_contents($file);
-            $decoded = json_decode($contents, true);
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                $contents = file_get_contents($file);
+                $decoded = json_decode($contents, true);
 
-            if (json_last_error() !== JSON_ERROR_NONE) {
-                $data[] = [
-                    'success' => false,
-                    'filename' => basename($file),
-                    'content' => null,
-                    'error' => 'Invalid JSON string ' . json_last_error_msg()
-                ];
+                if (json_last_error() !== JSON_ERROR_NONE) {
+                    $data[] = [
+                        'success' => false,
+                        'filename' => basename($file),
+                        'content' => null,
+                        'error' => 'Invalid JSON string ' . json_last_error_msg()
+                    ];
+                } elseif (json_last_error() === JSON_ERROR_NONE) {
+                    $data[] = [
+                        'success' => true,
+                        'filename' => basename($file),
+                        'content' => $decoded,
+                        'error' => null
+                    ];
+                }
             }
-
-            if (json_last_error() === JSON_ERROR_NONE) {
-                $data[] = [
-                    'success' => true,
-                    'filename' => basename($file),
-                    'content' => $decoded,
-                    'error' => null
-                ];
-            }
+        } else {
+            $data[] = [
+                'success' => false,
+                'filename' => null,
+                'content' => null,
+                'error' => 'Unable to find any files in "' . $this->directory . '"! Directory does not exist'
+            ];
         }
 
         return $data;
