9fbf0b026b36fa4c5d2b84e7892a4f492cb02cb8
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2025-11-28 23:49:15 -0500

    Update Session Authentication, Auth User Interface, Session Interface, Mail Service Provider, Add User Identity

--- src/Contracts/Authentication/AuthUserInterface.php
+++ src/Contracts/Authentication/AuthUserInterface.php
@@ -4,11 +4,19 @@ namespace JDS\Contracts\Authentication;
 
 interface AuthUserInterface
 {
-	public function getAuthId(): int|string;
+	public function getAuthId(): string;
+
+    public function getCompanyId(): string;
 
 	public function getEmail(): string;
 
 	public function getPassword(): string;
+
+    public function getRoleId(): string;
+
+    public function getPermisisonId(): string;
+
+    public function getAccessLevel(): int;
 
     public function isAdmin(): bool;
 }
