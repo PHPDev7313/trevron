1969685b7b48300bea446f3609a88208d1268e0a
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2025-12-03 01:09:03 -0500

    Add Framework Boot Strapper

---
+++ src/Configuration/FrameworkBootStrapper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace JDS\Configuration;
+
+use JDS\Console\ConsoleRuntimeException;
+use JDS\ServiceProvider\ConfigServiceProvider;
+use JDS\ServiceProvider\ConsoleServiceProvider;
+use JDS\ServiceProvider\DatabaseConsoleServiceProvider;
+use JDS\ServiceProvider\DatabaseServiceProvider;
+use JDS\ServiceProvider\LoggingServiceProvider;
+use League\Container\Container;
+
+class FrameworkBootStrapper
+{
+    public static function boot(Container $container): void
+    {
+        //
+        // Providers that MUST be fully registered EARLY
+        //
+        $coreProviders = [
+            ConfigServiceProvider::class,
+            LoggingServiceProvider::class,
+            DatabaseServiceProvider::class,
+            DatabaseConsoleServiceProvider::class,
+            ConsoleServiceProvider::class,
+        ];
+
+        foreach ($coreProviders as $coreClass) {
+
+            if (!$container->has($coreClass)) {
+                //
+                // provider was not added
+                //
+                throw new ConsoleRuntimeException("Core provider '{$coreClass}' was not registerd in services. Missing Core provider!");
+            }
+
+            //
+            // Resolve provider instance
+            //
+            $provider = $container->get($coreClass);
+
+            //
+            // Force register() to run NOW
+            //
+            if (!method_exists($provider, 'register')) {
+                throw new ConsoleRuntimeException("Core provider {$coreClass} was not registerd in services. Register method missing!");
+            }
+
+            $provider->register();
+            //
+            // Optional: support a "boot" lifecycle
+            //
+            if (method_exists($provider, 'boot')) {
+                $provider->boot();
+            }
+        }
+    }
+}
+
