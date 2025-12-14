ef038ec142e854f11b88ea3532cca2d84538005b
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2025-12-13 20:42:53 -0500

    Update ProcessRoutes for routes

--- src/Routing/ProcessRoutes.php
+++ src/Routing/ProcessRoutes.php
@@ -9,8 +9,6 @@ class ProcessRoutes
 {
     public static function process(array $routes): array
     {
-        $separateRoutes = [];
-
         $metadataList = [];
 
         foreach ($routes as $route) {
@@ -87,24 +85,4 @@ class ProcessRoutes
         ];
     }
 }
-
-
-//            $currentMetadata = [];
-//
-//            // check if the route has metadata in the expected position (index2[3])
-//            if (isset($route[2][3])) {
-//                // extract metadata and link back to the route URI (index 1)
-//                $currentMetadata = array_merge(['uri' => $route[1]], $route[2][3]);
-//                $metadataList[] = $currentMetadata;
-//                // remove metadata from the original route
-//                unset($route[2][3]);
-//            }
-//            $separateRoutes[] = $route;
-//        }
-//        return [
-//            'routes' => $separateRoutes,
-//            'metadata' => $metadataList,
-//        ];
-
-
 
