93c42798ac4448959507929c7c6d613afe44267f
Mr J <61154728+PHPDev7313@users.noreply.github.com>
2026-01-02 18:05:49 -0500

    Update Command Phase, Vendor files

--- src/Bootstrap/Phase/CommandPhase.php
+++ src/Bootstrap/Phase/CommandPhase.php
@@ -5,6 +5,7 @@ namespace JDS\Bootstrap\Phase;
 use JDS\Contracts\Bootstrap\BootstrapPhase;
 use JDS\Contracts\Bootstrap\BootstrapPhaseInterface;
 use JDS\Contracts\Console\CommandRegistryInterface;
+use JDS\Exceptions\Bootstrap\BootstrapInvariantViolationException;
 use League\Container\Container;
 
 class CommandPhase implements BootstrapPhaseInterface
@@ -18,7 +19,9 @@ class CommandPhase implements BootstrapPhaseInterface
     public function bootstrap(Container $container): void
     {
         if (!$container->has(CommandRegistryInterface::class)) {
-            return;
+            throw new BootstrapInvariantViolationException(
+                "Command Registry Interface is missing during COMMAND phase. [Command:Phase]."
+            );
         }
 
         $registry = $container->get(CommandRegistryInterface::class);
